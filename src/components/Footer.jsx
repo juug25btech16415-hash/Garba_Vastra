95ef5b1efa71a356bf1a86c2737c8325e7605dee
Vritika rajani <juug25btech16415@jainuniversity.ac.in>
2026-08-18 01:06:14 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,14 @@ export default function Footer() {
           alt="Garba Vastra"
           className="h-12 md:h-14 w-auto object-contain grayscale opacity-80 hover:grayscale-0 transition-all"
         />
+        
         <p>Contact for any queries: +91 9712029713</p>
-        <p>Handpicked chaniya cholis, made for the raas.</p>
+        
+        <div className="text-center sm:text-right flex flex-col gap-1">
+          <p>Handpicked chaniya cholis, made for the raas.</p>
+          <p>Made in Gujarat, India</p>
+          <p>From our house to yours</p>
+        </div>
       </div>
     </footer>
   )
