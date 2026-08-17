833b6da17dd97aa715702608c51e145120f203d2
Vritika rajani <juug25btech16415@jainuniversity.ac.in>
2026-08-18 01:11:43 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,26 @@ export default function Footer() {
   return (
     <footer className="border-t border-maroon/10 mt-16">
       <div className="max-w-6xl mx-auto px-5 py-8 flex flex-col sm:flex-row items-center justify-between gap-3 text-sm text-ink/50">
+        
+        {/* 1. Left Side: Logo */}
         <img
           src="/main-logo.png"
           alt="Garba Vastra"
           className="h-12 md:h-14 w-auto object-contain grayscale opacity-80 hover:grayscale-0 transition-all"
         />
-         
-        <div className="text-center sm:text-right flex flex-col gap-1">
-        <p>Contact for any queries: +91 9712029713</p>
-        <p>Email: vritika110@gmal.com</p>
         
+        {/* 2. Middle: Contact Info */}
+        <div className="text-center flex flex-col gap-1">
+          <p>Contact for any queries: +91 9712029713</p>
+          <p>Email: vritika110@gmail.com</p>
+        </div>
+        
+        {/* 3. Right Side: Taglines */}
         <div className="text-center sm:text-right flex flex-col gap-1">
           <p>Handpicked chaniya cholis, made for the raas.</p>
           <p>Made in Gujarat, India</p>
         </div>
+
       </div>
     </footer>
   )
