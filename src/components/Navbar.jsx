9288cb8ea25b3cf48d1d5c4267320f92b46a808d
Vritika rajani <juug25btech16415@jainuniversity.ac.in>
2026-08-18 00:58:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
           <img
             src="/main-logo.png"
             alt="Garba Vastra"
-            className="h-12 md:h-16 w-auto object-contain"
+            className="h-16 md:h-18 w-auto object-contain"
           />
         </Link>
 
