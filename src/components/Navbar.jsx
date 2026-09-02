73d5ffc60c05eb1700fb7c3d63aaeac98757c98a
Vritika rajani <juug25btech16415@jainuniversity.ac.in>
2026-09-02 23:35:32 +0530

    ui: camouflage logo background using mix-blend-multiply

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ export default function Navbar() {
             <img
               src="/main-logo.png"
               alt="Garba Vastra"
-              className="h-14 sm:h-16 md:h-18 w-auto object-contain"
+              className="h-14 sm:h-16 md:h-18 w-auto object-contain mix-blend-multiply"
             />
           </Link>
         </div>
