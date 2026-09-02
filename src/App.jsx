a0148f2fa1dccb1f86f438cf8b712c44c28efbfe
Vritika rajani <juug25btech16415@jainuniversity.ac.in>
2026-09-03 01:07:03 +0530

    fix: optimize mobile navbar UI and implement scroll restoration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, ScrollRestoration } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -16,6 +16,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollRestoration />
       <Navbar />
       <main className="flex-1">
         <Routes>
